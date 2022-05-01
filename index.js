0883c15e60897cc67144c4cc3ffbb820be3436ef
Dhruv Garg <dhruv.garg@innovaccer.com>
2022-05-01 13:38:01 +0530

    Added Career Page

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+
+const app =express()
+
+app.get('/',(req,res)=>{
+    console.log("Index Page")
+    res.send("<h1>WELCOME TO THE NODE JS APP</h1>")
+})
+
+app.get('/about',(req,res)=>{
+    console.log("About Page")
+    res.send("<h1>WELCOME TO THE About Page of NODE JS APP</h1>")
+})
+
+app.get('/career',(req,res)=>{
+    console.log("Career Page")
+    res.send("<h1>WELCOME TO THE Career Page of NODE JS APP</h1>")
+})
+
+app.listen(3000)
