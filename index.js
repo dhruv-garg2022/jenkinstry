1693068e0cf8bb00feb5b063062fb169e203100d
Dhruv Garg <dhruv.garg@innovaccer.com>
2022-05-01 13:53:50 +0530

    Added New Page

--- index.js
+++ index.js
@@ -17,4 +17,9 @@ app.get('/career',(req,res)=>{
     res.send("<h1>WELCOME TO THE Career Page of NODE JS APP</h1>")
 })
 
+app.get('/new',(req,res)=>{
+    console.log("New Page")
+    res.send("<h1>WELCOME TO THE New Page of NODE JS APP</h1>")
+})
+
 app.listen(3000)
